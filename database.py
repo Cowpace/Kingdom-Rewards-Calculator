b0d724347ab09d1ee48e148ba574af6c754434f0
Cowpace <kyleurq@knights.ucf.edu>
2016-04-19 18:41:34 -0400

    Now plots the data!

--- database.py
+++ database.py
@@ -26,6 +26,13 @@ INSERT_INTO_KINGDOM_TABLE_QUERY = """
     INSERT INTO """ + DB_NAME + """ ({}) VALUES (?,?,?,?,?,?,?,?,?,?,?)
 """
 
+MAIN_KINGDOM_QUERY = """
+    SELECT *
+    FROM
+        (SELECT """ + ",".join(KINGDOM_TABLE_COL_LIST[:-1]) + """ FROM Kingdom ORDER BY date desc {})
+    ORDER BY date asc
+"""
+
 class KingdomDB(object):
     def __init__(self):
         self.conn = sqlite3.connect(DB_PATH)
@@ -33,12 +40,35 @@ class KingdomDB(object):
     def __del__(self):
         self.conn.close()
 
+    def get_cursor(self):
+        return self.conn.cursor()
+
     def insert_data(self, dict):
         li = dict.values()
-        c = self.conn.cursor()
+        c = self.get_cursor()
         print INSERT_INTO_KINGDOM_TABLE_QUERY.format(",".join(dict.keys()))
         c.execute(INSERT_INTO_KINGDOM_TABLE_QUERY.format(",".join(dict.keys())), tuple(li))
         self.conn.commit()
+
+    def query_plottable(self, limit = None):
+        if (limit is None):
+            limit = ""
+        elif (type(limit) == int):
+            limit = "LIMIT " + str(limit)
+        else:
+            raise Exception("limit is wrong type. Must be int or Nonetype")
+
+        q = MAIN_KINGDOM_QUERY.format(limit)
+        c = self.get_cursor()
+        print q
+        c.execute(q)
+        rows = c.fetchall()
+        data = {k : [] for k in KINGDOM_TABLE_COL_LIST[:-1]}
+        for row in rows:
+            for i, col in enumerate(row):
+                data[KINGDOM_TABLE_COL_LIST[i]].append(col)
+        return data
+
 
 
 if __name__ == '__main__':
---
+++ graph.py
@@ -0,0 +1,23 @@
+import matplotlib
+from matplotlib import pyplot
+from database import KingdomDB
+
+def draw_data():
+    GRAPH_OPTIONS = [c + "o-" for c in "bgrcmyk"] + [c + "^--" for c in "bgrcmyk"]
+
+    COST = 350000
+
+    db = KingdomDB()
+    dict = db.query_plottable()
+
+    for i, (k, v) in enumerate(dict.iteritems()):
+        if (k == "date"):
+            continue
+        p = pyplot.plot([elem - COST for elem in v], GRAPH_OPTIONS[i-1])
+
+    db.conn.close()
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    draw_data()
